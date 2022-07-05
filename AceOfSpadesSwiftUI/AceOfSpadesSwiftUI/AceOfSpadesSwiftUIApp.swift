78f811ba3d2f4e0205a816239b4d6072052298ab
Yuliya Grygoryeva <jgrygoryeva@gmail.com>
2022-07-05 15:25:16 -0700

    Initial commit

---
+++ AceOfSpadesSwiftUI/AceOfSpadesSwiftUI/AceOfSpadesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AceOfSpadesSwiftUIApp.swift
+//  AceOfSpadesSwiftUI
+//
+//  Created by Ivan Ramirez on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct AceOfSpadesSwiftUIApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ AceOfSpadesSwiftUI/AceOfSpadesSwiftUI/View/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  AceOfSpadesSwiftUI
+//
+//  Created by Ivan Ramirez on 6/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, World!")
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
